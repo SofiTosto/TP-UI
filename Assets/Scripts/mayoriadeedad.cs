d68433e17e11a80c2be624f996ceba3f7837265e
SofiTosto <sofiatosto@gmail.com>
2022-05-26 02:31:24 -0300

    Initial commit

---
+++ Assets/Scripts/mayoriadeedad.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mayoriadeedad : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+     public int edadusu;
+     public Text mitexto;
+
+    void Start()
+    {
+        
+        if (edadusu >= 18)
+        {
+            Debug.Log("Es mayor de edad");
+            mitexto.text = "Es mayor de edad";
+        }
+        else
+        {
+            Debug.Log("No es mayor de edad");
+            mitexto.text = "No es mayor de edad";
+        }
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
